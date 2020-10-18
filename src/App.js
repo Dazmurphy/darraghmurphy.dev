fda94f59598a82702e88d1a6d2d691a5fc98d036
dazmurphy <daz.murphy.94@gmail.com>
2020-10-18 12:09:32 +0100

    init react-app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://avatars1.githubusercontent.com/u/10632384?s=460&u=6d130fbe13a1603f5e25c13bf680a18653531147&v=4" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
