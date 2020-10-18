2019fc10ec57cadaf956264ad01fb28d3314fd47
dazmurphy <daz.murphy.94@gmail.com>
2020-10-18 12:15:19 +0100

    Updates

---
+++ precache-manifest.7e65f963d5d85bcae9815197d3fc3149.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36713b3f0012c96744a2847c31f649d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40c6a9edad70f9a548cf",
+    "url": "/static/css/main.453e7531.chunk.css"
+  },
+  {
+    "revision": "fe95718e7320b45c4600",
+    "url": "/static/js/2.82fd186f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.82fd186f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c6a9edad70f9a548cf",
+    "url": "/static/js/main.938ba06b.chunk.js"
+  },
+  {
+    "revision": "91786065b74a40968aa2",
+    "url": "/static/js/runtime-main.57727732.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
