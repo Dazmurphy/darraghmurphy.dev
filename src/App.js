670d843c575ee9f89d3c5378be0107e6540cd1e2
dazmurphy <daz.murphy.94@gmail.com>
2020-10-27 14:02:04 +0000

    Updated header links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Layout, Menu, Breadcrumb } from 'antd';
+import { GithubOutlined, LinkedinFilled, HomeFilled, FundFilled, FileTextFilled } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 
 const { Header, Footer, Sider, Content } = Layout;
@@ -11,9 +12,11 @@ function App() {
     <Header>
       <div className="logo" />
       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-        <Menu.Item key="1">Home</Menu.Item>
-        <Menu.Item key="2">LinkedIn</Menu.Item>
-        <Menu.Item key="3">Github</Menu.Item>
+        <Menu.Item icon={<HomeFilled />} key="1">Home</Menu.Item>
+        <Menu.Item icon={<FileTextFilled />} key="2">Blog</Menu.Item>
+        <Menu.Item icon={<LinkedinFilled />} key="3"><a href="https://www.linkedin.com/in/darraghmurphycs/" target="_blank" rel="noopener noreferrer">LinkedIn</a></Menu.Item>
+        <Menu.Item icon={<GithubOutlined />} key="4"><a href="https://github.com/dazmurphy" target="_blank" rel="noopener noreferrer">Github</a></Menu.Item>
+        <Menu.Item icon={<FundFilled />} key="5"><a href="#" >Stocks</a></Menu.Item>
       </Menu>
     </Header>
     <Layout>
@@ -28,7 +31,7 @@ function App() {
     </Content>
       <Sider>right sidebar</Sider>
     </Layout>
-    <Footer style={{ textAlign: 'center' }}>Darragh Murphy ©2020 Created by Darragh Murphy</Footer>
+    <Footer style={{ textAlign: 'center' }}>darraghmurphy.dev ©2020 Created by Darragh Murphy</Footer>
   </Layout>
   );
 }
